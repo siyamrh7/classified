6952fa002f39f619121e5f3dbe691a3df09b759d
siyamrh7 <siyamrh7@gmail.com>
2021-12-06 08:49:20 -0400

    some

--- controllers/AuthCtrl.js
+++ controllers/AuthCtrl.js
@@ -1,50 +1,50 @@
-const Users=require('../models/User')
-const bcrypt=require('bcrypt')
-const jwt=require('jsonwebtoken')
+const Users = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-const registerCtrl=async(req,res)=>{
-  const {name,email,password}=req.body
-  if(!email || !name || !password){
-      return res.json({status:false,msg:"Invalid creadentials"})
-  }
-  try {
-    const check= await Users.findOne({email})
-    if(check){
-        return res.json({status:false,msg:"User already exist"})
-    }
-    const hashpass=await bcrypt.hash(password,11)
-    const User= new Users({
-        name,email,password:hashpass
-    })
-    const user=await User.save()
-    const token=await jwt.sign({user},process.env.JWT_SECRET,{expiresIn:'7d'})
-    res.json({status:true,msg:"Registration Successfull",token})
-  } catch (error) {
-      res.json({status:false,msg:error.message})
-  }
-}
-
-
-const loginCtrl=async(req,res)=>{
-    const {email,password}=req.body
-    if(!email || !password){
-        return res.json({status:false,msg:"Invalid creadentials"})
+const registerCtrl = async (req, res) => {
+    const { name, email, password } = req.body
+    if (!email || !name || !password) {
+        return res.json({ status: false, msg: "Invalid creadentials" })
     }
     try {
-        const user= await Users.findOne({email})
-    if(!user){
-        return res.json({status:false,msg:"User doesn't exist"})
-    }
-    const check=await bcrypt.compare(password,user.password)
-    if(!check){
-        return res.json({status:false,msg:"Password doesn't matched"})
-    }
-    const token=await jwt.sign({user},process.env.JWT_SECRET,{expiresIn:'7d'})
-    res.json({status:true,msg:"Login Successfull",token})
+        const check = await Users.findOne({ email })
+        if (check) {
+            return res.json({ status: false, msg: "User already exist" })
+        }
+        const hashpass = await bcrypt.hash(password, 11)
+        const User = new Users({
+            name, email, password: hashpass
+        })
+        const user = await User.save()
+        const token = await jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '7d' })
+        res.json({ status: true, msg: "Registration Successfull", token })
     } catch (error) {
-        res.json({status:false,msg:error.message})
+        res.json({ status: false, msg: error.message })
     }
 }
 
 
-module.exports={registerCtrl,loginCtrl}
+const loginCtrl = async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.json({ status: false, msg: "Invalid creadentials" })
+    }
+    try {
+        const user = await Users.findOne({ email })
+        if (!user) {
+            return res.json({ status: false, msg: "User doesn't exist" })
+        }
+        const check = await bcrypt.compare(password, user.password)
+        if (!check) {
+            return res.json({ status: false, msg: "Password doesn't matched" })
+        }
+        const token = await jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '7d' })
+        res.json({ status: true, msg: "Login Successfull", token })
+    } catch (error) {
+        res.json({ status: false, msg: error.message })
+    }
+}
+
+
+module.exports = { registerCtrl, loginCtrl }
--- routes/Routes.js
+++ routes/Routes.js
@@ -1,10 +1,10 @@
-const express=require('express')
-const {registerCtrl,loginCtrl}=require('../controllers/AuthCtrl')
-const router=express.Router()
+const express = require('express')
+const { registerCtrl, loginCtrl } = require('../controllers/AuthCtrl')
+const router = express.Router()
 
-router.post('/api/signup',registerCtrl)
-router.post('/api/signin',loginCtrl)
-router.get('/',(req,res)=>{
+router.post('/api/signup', registerCtrl)
+router.post('/api/signin', loginCtrl)
+router.get('/', (req, res) => {
     res.send("Server is running")
 })
-module.exports=router
+module.exports = router
